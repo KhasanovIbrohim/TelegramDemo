53f804dfbc2240018d348b60dbfdb8fdf0acfcde
Khasanov Ibrohim <104366293+KhasanovIbrohim@users.noreply.github.com>
2023-03-09 16:38:15 +0500

    1.0

---
+++ script/auto.js
@@ -0,0 +1,9 @@
+console.log(navigator.userAgent)
+
+
+const div = document.querySelector('.box')
+
+div.style.fontSize = "40px"
+div.style.fontWeight = "bolder"
+
+div.textContent = navigator.userAgent
