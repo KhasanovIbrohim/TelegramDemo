7d7d508abe87b9f9d225c5b5f458545b8be0170f
Khasanov Ibrohim <104366293+KhasanovIbrohim@users.noreply.github.com>
2023-03-10 22:04:29 +0500

    2.1

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@ const arrowLeft = document.querySelector('.arrow-left')
 const arrowRight = document.querySelector('.arrow-right')
 const chats = document.querySelector('.left')
 const messages = document.querySelector('.right')
+const arrowChatL = document.querySelector('.arrow-chat-l')
+const arrowChatR = document.querySelector('.arrow-chat-r')
 
 function openChats(){
     arrowLeft.style.display = "none"
@@ -15,4 +17,18 @@ function closeChats(){
     arrowRight.style.display = "none"
     chats.style.display = "none"
     messages.style.width = "100%"
+}
+
+function openMessages(){
+    arrowLeft.style.display = "block"
+    arrowRight.style.display = "none"
+    chats.style.display = "none"
+    messages.style.width = "100%"   
+}
+
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    chats.style.width = "100%";
+    messages.style.display = "none";
+} else {
+    console.log("OK")
 }
