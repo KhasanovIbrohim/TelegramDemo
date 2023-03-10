031b51d3fd77f81de1d01013dd07097f3d99c8b1
Khasanov Ibrohim <104366293+KhasanovIbrohim@users.noreply.github.com>
2023-03-10 22:10:17 +0500

    2.1.2

--- script/script.js
+++ script/script.js
@@ -2,8 +2,8 @@ const arrowLeft = document.querySelector('.arrow-left')
 const arrowRight = document.querySelector('.arrow-right')
 const chats = document.querySelector('.left')
 const messages = document.querySelector('.right')
-const arrowChatL = document.querySelector('.arrow-chat-l')
-const arrowChatR = document.querySelector('.arrow-chat-r')
+const arrowLeftM = document.querySelector('.arrow-left-m')
+const arrowRightM = document.querySelector('.arrow-right-m')
 
 function openChats(){
     arrowLeft.style.display = "none"
@@ -27,9 +27,30 @@ function openMessages(){
     messages.style.width = "100%"  
 }
 
+function openChatsM(){
+    arrowLeftM.style.display = "none"
+    arrowRightM.style.display = "block"
+    chats.style.display = "block"
+    messages.style.width = "100%"
+}
+
+function closeChatsM(){
+    arrowLeftM.style.display = "block"
+    arrowRightM.style.display = "none"
+    chats.style.display = "none"
+    messages.style.width = "100%"
+}
+
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     chats.style.width = "100%";
     messages.style.display = "none";
+    arrowRight.style.display = "none";
+    arrowLeft.style.display = "none";
+    arrowRightM.style.display = "block";
+    arrowLeftM.style.display = "block";
 } else {
-    console.log("OK")
+    arrowRight.style.display = "block";
+    arrowLeft.style.display = "block";
+    arrowRightM.style.display = "none";
+    arrowLeftM.style.display = "none";
 }
